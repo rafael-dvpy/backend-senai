d6029f7e4596e6b22ebdff6773a84d7712979309
Rafael <rafa.dev.oliveira@gmail.com>
2023-06-26 23:55:02 +0000

    Player services

--- src/services/campoServices.js
+++ src/services/campoServices.js
@@ -7,3 +7,9 @@ export const getCampoById = (req, res) => {
 
 export const createNewCampo = (req, res) => {
 }
+
+export const updateCampoById = (req, res) => {
+}
+
+export const deleteCampoById = (req, res) => {
+}
--- src/services/playerServices.js
+++ src/services/playerServices.js
@@ -1,11 +1,51 @@
-import { PlayerModel } from "../models/playerModel.js";
+import {PlayerModal} from "../models/playerModel.js"
 
-export const getAllPlayers = (req, res) => {}
+export const getAllPlayers = async (req, res) => {
+    const players = await PlayerModal.find()
+    res.send(players)
+}
 
-export const getPlayerById = (req, res) => {}
+export const getPlayerById = async (req, res) => {
+    const id = req.params.id
+    const player = await PlayerModal.findById(id)
+    res.send(player)
+}
 
-export const createPlayer = (req, res) => {}
+export const createPlayer = async (req, res) => {
+    const playerData = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        campos: [],
+        createdAt: new Date().toUTCString(),
+    }
 
-export const updatePlayerById = (req, res) => {}
+    const newPlayer = new PlayerModal(playerData)
 
-export const deletePlayerById = (req, res) => {}
+    await newPlayer.save()
+
+    res.send(playerData)
+}
+
+export const updatePlayerById = (req, res) => {
+    const playerData = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        campos: req.body.campos,
+    }
+
+    const id = req.params.id
+
+    PlayerModal.findByIdAndUpdate(id, playerData)
+
+    res.send(playerData)
+}
+
+export const deletePlayerById = (req, res) => {
+    const id = req.params.id
+
+    PlayerModal.findByIdAndDelete(id)
+
+    res.send("Deleted")
+}
